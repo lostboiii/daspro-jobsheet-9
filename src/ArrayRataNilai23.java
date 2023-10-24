aa37ddc2c8464087fb009d6975691309d8b01d86
daoatzy <dafasetiadi65@gmail.com>
2023-10-24 13:05:01 +0700

    modifikasi jawaban

--- src/ArrayRataNilai23.java
+++ src/ArrayRataNilai23.java
@@ -3,16 +3,26 @@ import java.util.Scanner;
 public class ArrayRataNilai23 {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
-         int[] nilaiMhs = new int[10];
+         System.out.print("Masukkan jumlah  mahasiswa : ");
+         int n = sc.nextInt();
+         int[] arr = new int[n];
          double total = 0;
          double rata2;
-         for (int i = 0; i < nilaiMhs.length; i++) {
+         for (int i = 0; i < arr.length; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" :");
-            nilaiMhs[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
          }
-         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+         for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
          }
+         for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 70) {
+               System.out.println("Mahasiswa ke-"+(i+1)+" lulus!");
+           }
+           else{
+               System.out.println("Mahasiswa ke-"+(i+1)+" tidak lulus!");
+           }
+       }
          rata2 = total/2;
          System.out.println("Rata rata nilai = "+ rata2);
          sc.close();
