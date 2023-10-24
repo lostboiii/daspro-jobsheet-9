3a2bd486a691760497a01e173ee6e6c51640d019
daoatzy <dafasetiadi65@gmail.com>
2023-10-24 15:36:02 +0700

    modifikasi jawaban fix

--- src/ArrayRataNilai23.java
+++ src/ArrayRataNilai23.java
@@ -5,26 +5,42 @@ public class ArrayRataNilai23 {
          Scanner sc = new Scanner(System.in);
          System.out.print("Masukkan jumlah  mahasiswa : ");
          int n = sc.nextInt();
-         int[] arr = new int[n];
-         double total = 0;
-         double rata2;
-         for (int i = 0; i < arr.length; i++) {
+
+         int[] nilaiMhs = new int[n];
+         double totalLulus = 0;
+         double totalTidakLulus = 0;
+         double rata2Lulus;
+         double rata2TidakLulus;
+         int lulusCount = 0;
+         int tidakLulusCount = 0;
+         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" :");
-            arr[i] = sc.nextInt();
+            nilaiMhs[i] = sc.nextInt();
+             if (nilaiMhs[i] > 70) {
+            lulusCount++;
+            totalLulus += nilaiMhs[i];
+        }
+        else{
+            tidakLulusCount++;
+            totalTidakLulus += nilaiMhs[i];
+        }
          }
-         for (int i = 0; i < arr.length; i++) {
-            total += arr[i];
-         }
-         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 70) {
-               System.out.println("Mahasiswa ke-"+(i+1)+" lulus!");
-           }
-           else{
-               System.out.println("Mahasiswa ke-"+(i+1)+" tidak lulus!");
-           }
-       }
-         rata2 = total/2;
-         System.out.println("Rata rata nilai = "+ rata2);
+        
+    //      for (int i = 0; i < nilaiMhs.length; i++) {
+    //         if (nilaiMhs[i] > 70) {
+    //            System.out.println("Mahasiswa ke-"+(i+1)+" lulus!");
+    //        }
+    //        else{
+    //            System.out.println("Mahasiswa ke-"+(i+1)+" tidak lulus!");
+    //        }
+    //    }
+       rata2Lulus = totalLulus/lulusCount;
+       rata2TidakLulus = totalTidakLulus/tidakLulusCount;
+       
+       System.out.println("Rata-rata nilai mahasiswa yang lulus: "+ rata2Lulus);
+       System.out.println("Rata-rata nilai mahasiswa yang tidak lulus: "+ rata2TidakLulus);
+        //  rata2 = total/2;
+        //  System.out.println("Rata rata nilai = "+ rata2);
          sc.close();
     }
     
