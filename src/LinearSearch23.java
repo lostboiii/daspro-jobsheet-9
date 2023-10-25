f353436e718e7def698850b1e18a750bce5caeb0
daoatzy <dafasetiadi65@gmail.com>
2023-10-25 18:51:30 +0700

    first commit

---
+++ src/LinearSearch23.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class LinearSearch23 {
+    public static void main(String[] args) {
+       
+        int key = 0;
+        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+         System.out.print("Masukkan jumlah  array : ");
+         int n = sc.nextInt();
+         int[] arr = new int[n];
+         for (int i = 0; i < arr.length; i++) {
+            System.out.println("Masukkan nilai array ke-"+(i+1)+" :");
+            arr[i] = sc.nextInt();
+         }
+         System.out.println("masukkan key yang ingin dicari : ");
+         key = sc.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                hasil = i;
+                break;
+            }
+
+        }
+        if (hasil != 0) {
+            System.out.println("Key ada di posisi indeks ke-" + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan.");
+        }
+        sc.close();
+    }
+}
